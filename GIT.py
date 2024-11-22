3de7ccb582a0f6feb8b75d7e6dec3c894b93256b
Tshering Dorji <6710301001@cdti.ac.th>
2024-11-22 16:06:25 +0700

    New feature

--- GIT.py
+++ GIT.py
@@ -1,4 +1,15 @@
 name = input("What is your name? :")
 print(f"hello!! {name}")
 age = input("How Old are you?")
-print(f"you are {age} years old") 
+print(f"you are {age} years old")
+
+def Multiply(x,y):
+    product = x * y
+
+    return product
+
+num1 = int(input("Enter number 1 :"))
+num2 = int(input("Enter number 2 :"))
+result = Multiply(num1,num2)
+print(f"result is {result}")
+
